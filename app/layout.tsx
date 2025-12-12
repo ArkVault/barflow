46764c31f72f00fe1e2e777cfb62c028d8ccb9c2
Ark Vault <77123356+ArkVault@users.noreply.github.com>
2025-12-12 14:32:13 -0600

    fix: Add LanguageProvider to root layout
    - Wrap entire application with LanguageProvider
    - This enables all components to access language context
    - Language toggle now properly updates all components
    - Fixed the issue where translations weren't updating on toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/contexts/auth-context'
 import { SubscriptionGuard } from '@/components/subscription-guard'
+import { LanguageProvider } from '@/hooks/use-language'
 import { Toaster } from 'sonner'
 
 const geist = Geist({
@@ -51,12 +52,14 @@ export default function RootLayout({
         />
       </head>
       <body className={`${geist.variable} ${geistMono.variable} font-sans antialiased`}>
-        <AuthProvider>
-          <SubscriptionGuard>
-            {children}
-          </SubscriptionGuard>
-          <Toaster position="top-right" richColors />
-        </AuthProvider>
+        <LanguageProvider>
+          <AuthProvider>
+            <SubscriptionGuard>
+              {children}
+            </SubscriptionGuard>
+            <Toaster position="top-right" richColors />
+          </AuthProvider>
+        </LanguageProvider>
       </body>
     </html>
   )
